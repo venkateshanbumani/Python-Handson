9d33473379691743a06893608cde17025c092ab5
Venkatesh <122010023+venkateshanbumani@users.noreply.github.com>
2024-12-25 09:55:06 +0530

    Created upper_lower_case.py

---
+++ Basic Programs/upper_lower_case.py
@@ -0,0 +1,10 @@
+text = "Learn Python Programming"
+
+uppercase = text.upper()
+lowercase = text.lower()
+
+print ("Uppercase:",uppercase)
+print ("Lowercase:",lowercase)
+
+print (f"Uppercase:{uppercase}")
+print (f"Lowercase:{lowercase}")
